85b58667cba58b7b2a6d3b555942fd009803ff0b
Martin Schrimpf <m4rtinsch@gmail.com>
2019-09-29 17:14:27 -0400

    retrieve weights and units for tensorflow

--- model_tools/activations/tensorflow.py
+++ model_tools/activations/tensorflow.py
@@ -1,6 +1,10 @@
 from collections import OrderedDict
+from pathlib import Path
 
-from model_tools.activations.core import ActivationsExtractorHelper
+import numpy as np
+from xarray import DataArray
+
+from model_tools.activations.core import ActivationsExtractorHelper, collapse_weights, merge_weight_assemblies
 
 
 class TensorflowWrapper:
@@ -30,6 +34,100 @@ class TensorflowWrapper:
                                     for layer in layer_names)
         layer_outputs = self._session.run(layer_tensors, feed_dict={self._inputs: images})
         return layer_outputs
+
+    @property
+    def weights(self):
+        import tensorflow as tf
+        weights = []
+        for variable in tf.trainable_variables():
+            values = self._session.run(variable)  # shape: kernel_x x kernel_y x input x output
+            # transform into outputs, inputs, kernel_x, kernel_y
+            if len(values.shape) == 4:  # convolutional
+                values = np.transpose(values, [3, 2, 0, 1])
+            elif len(values.shape) == 2:  # dense
+                values = np.transpose(values, [1, 0])
+            elif len(values.shape) == 1:  # bias
+                pass
+            else:
+                raise ValueError(f"Unknown variable shape length: {values.shape}")
+            values, flatten_coords = collapse_weights(values)
+            parameter_weights = DataArray(values, coords={
+                **{'name': ('weights', [variable.name] * len(values)),
+                   },
+                **{coord: ('weights', coord_values) for coord, coord_values in flatten_coords.items()},
+            }, dims=['weights'])
+            weights.append(parameter_weights)
+        weights = merge_weight_assemblies(weights)
+        return weights
+
+    def list_ops(self):
+        import tensorflow as tf
+        include_prefix = "/"
+        exclude_prefix = include_prefix + "_"
+        ops = tf.get_default_graph().get_operations()
+        # ops = [op for op in ops if op.name.startswith(include_prefix)]
+        ops = [op for op in ops]
+        ops = [op for op in ops if not op.name.startswith(exclude_prefix)]
+        return ops
+
+    def _list_layers(self):
+        """Returns a list of (layer_name, output_expr, trainable_vars) tuples corresponding to
+        individual layers of the network. Mainly intended to be used for reporting."""
+        import tensorflow as tf
+        layers = []
+
+        def recurse(scope, parent_ops, parent_vars, level):
+            # Ignore specific patterns.
+            if any(p in scope for p in ["/Shape", "/strided_slice", "/Cast", "/concat", "/Assign"]):
+                return
+
+            # Filter ops and vars by scope.
+            global_prefix = (scope + "/") if scope else ''
+            cur_ops = [op for op in parent_ops if op.name.startswith(global_prefix) or op.name == global_prefix[:-1]]
+            cur_vars = [(name, var) for name, var in parent_vars
+                        if name.startswith(global_prefix) or name == global_prefix[:-1]]
+            if not cur_ops and not cur_vars:
+                return
+
+            # Filter out all ops related to variables.
+            for var in [op for op in cur_ops if op.type.startswith("Variable")]:
+                var_prefix = var.name + "/"
+                cur_ops = [op for op in cur_ops if not op.name.startswith(var_prefix)]
+
+            # Filter out init
+            cur_ops = [op for op in cur_ops if op.type != 'NoOp']
+
+            # Scope does not contain ops as immediate children => recurse deeper.
+            contains_direct_ops = any(
+                "/" not in op.name[len(global_prefix):] and op.type != "Identity" for op in cur_ops)
+            if (level == 0 or not contains_direct_ops) and (len(cur_ops) + len(cur_vars)) > 1:
+                visited = set()
+                for rel_name in [op.name[len(global_prefix):] for op in cur_ops] + \
+                                [name[len(global_prefix):] for name, _var in cur_vars]:
+                    token = rel_name.split("/")[0]
+                    if token not in visited:
+                        recurse(global_prefix + token, cur_ops, cur_vars, level + 1)
+                        visited.add(token)
+                return
+
+            # Report layer.
+            layer_name = scope
+            layer_output = cur_ops[-1].outputs[0] if cur_ops else cur_vars[-1][1]
+            layer_trainables = [var for _name, var in cur_vars if var.trainable]
+            layers.append((layer_name, layer_output, layer_trainables))
+
+        vars = OrderedDict((var.name, var) for var in tf.global_variables())
+        recurse('', self.list_ops(), list(vars.items()), 0)
+        return layers
+
+    @property
+    def units(self):
+        weight_layers = [layer_name for layer_name, layer_output, layer_trainables in self._list_layers()
+                               if sum(np.prod(var.shape) for var in layer_trainables) > 0]
+        # run dummy image
+        image_filepath = Path(__file__).parent / 'dummy.jpg'
+        activations = self.from_paths([str(image_filepath)], layers=weight_layers)
+        return DataArray(activations['neuroid'])
 
     def graph(self):
         import networkx as nx
--- tests/activations/test_weights_units.py
+++ tests/activations/test_weights_units.py
@@ -1,13 +1,15 @@
 import functools
 from xarray import DataArray
 import pytest
-from ..models import pytorch_custom, keras_vgg19
+from ..models import pytorch_custom, keras_vgg19, tfslim_custom, tfslim_vgg16
 
 
 @pytest.mark.parametrize(['model_ctr', 'expected_num_weights'], [
     (pytorch_custom, 98569056),
     (functools.partial(pytorch_custom, kernel_size=5), 96801152),
-    (keras_vgg19, 143667240),
+    pytest.param(keras_vgg19, 143667240, marks=pytest.mark.memory_intense),
+    (tfslim_custom, 1048296),
+    pytest.param(tfslim_vgg16, 138361641, marks=pytest.mark.memory_intense),
 ])
 def test_weights(model_ctr, expected_num_weights):
     model = model_ctr()
@@ -18,7 +20,9 @@ def test_weights(model_ctr, expected_num_weights):
 
 @pytest.mark.parametrize(['model_ctr', 'expected_num_units'], [
     (functools.partial(pytorch_custom, kernel_size=5), 97800),
-    (keras_vgg19, 14861288),
+    pytest.param(keras_vgg19, 14861288, marks=pytest.mark.memory_intense),
+    (tfslim_custom, 187624),
+    pytest.param(tfslim_vgg16, 13556713, marks=pytest.mark.memory_intense),
 ])
 def test_units(model_ctr, expected_num_units):
     model = model_ctr()
