976b42cc36cc8bfbab88dbf6c4c4bc01cd4cafd6
Jonathan Prescott-Roy <jjpr@mit.edu>
2021-06-23 12:59:32 -0400

    Update to use brainio_core package.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md') as readme_file:
     readme = readme_file.read()
 
 requirements = [
-    "brainio_base @ git+https://github.com/brain-score/brainio_base",
+    "brainio-core @ git+https://github.com/brain-score/brainio-core",
     "brain-score @ git+https://github.com/brain-score/brain-score",
     "h5py",
     "Pillow",
