4bbc634c1462d1985a54d9cc6b430e866837e587
Linus <95619282+linus-md@users.noreply.github.com>
2023-12-07 18:32:32 +0100

    Update behavior.py

--- model_tools/brain_transformation/behavior.py
+++ model_tools/brain_transformation/behavior.py
@@ -6,9 +6,7 @@ import pandas as pd
 import sklearn.linear_model
 import sklearn.multioutput
 from sklearn.metrics.pairwise import cosine_similarity
-
 from typing import Union, List
-
 from brainio.assemblies import walk_coords, array_is_element, BehavioralAssembly, DataAssembly
 from model_tools.utils import make_list
 from brainscore.model_interface import BrainModel
